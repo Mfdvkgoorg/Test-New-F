acbcae36e73a2988dac50bb77537b42f16a1fecd
Mfdvkgoorg <namwa226@gmail.com>
2026-07-21 01:14:34 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1475,7 +1475,7 @@ local aa = {
                 "TextButton",
                 {
                     Size = UDim2.new(1, 0, 0, 34),
-                    BackgroundTransparency = 0.92,
+                    BackgroundTransparency = 0.6, -- ทำให้สีพื้นหลัง tap main / setting มองเห็นชัดขึ้น
                     Parent = s,
                     ThemeTag = {BackgroundColor3 = "Tab"}
                 },
@@ -1562,7 +1562,7 @@ local aa = {
             j.AddSignal(
                 x.Frame.MouseEnter,
                 function()
-                    x.SetTransparency(x.Selected and 0.85 or 0.87)
+                    x.SetTransparency(x.Selected and 0.43 or 0.43)
                 end
             )
             j.AddSignal(
